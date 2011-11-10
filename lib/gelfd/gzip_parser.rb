bde68bfbe0eca96c3ff604729e8648fb1da41a74
John E. Vincent <lusis.org+github.com@gmail.com>
2011-11-10 16:23:20 -0500

    working gzip decoder

--- lib/gelfd/gzip_parser.rb
+++ lib/gelfd/gzip_parser.rb
@@ -5,7 +5,7 @@ module Gelfd
     def self.parse(data)
       begin
         t = Zlib::GzipReader.new(StringIO.new(data))
-        t
+        t.read
       #raise NotYetImplementedError, "GZip decoding is not yet implemented"
       rescue Exception => e
         raise DecodeError, "Failed to decode data: #{e}"
